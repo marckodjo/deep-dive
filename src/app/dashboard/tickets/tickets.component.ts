6d9bfdfa51ac30a336d98a54a351ce961edfe715
marckodjo <marc.amedonou@gmail.com>
2025-04-28 16:12:23 -0400

    end of section 6- deep dive

--- src/app/dashboard/tickets/tickets.component.ts
+++ src/app/dashboard/tickets/tickets.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { NewTicketComponent } from './new-ticket/new-ticket.component';
 import { Ticket } from './ticket.model';
-import { TicketComponent } from "./ticket/ticket.component";
+import { TicketComponent } from './ticket/ticket.component';
 
 @Component({
   selector: 'app-tickets',
@@ -14,12 +14,21 @@ export class TicketsComponent {
   tickets: Ticket[] = [];
 
   onAdd(ticketData: { title: string; text: string }) {
-    const ticket: Ticket ={
+    const ticket: Ticket = {
       title: ticketData.title,
       request: ticketData.text,
       id: Math.random().toString(),
-      status:'open'
-    }
+      status: 'open',
+    };
     this.tickets.push(ticket);
+  }
+
+  onCloseTicket(id: string) {
+    this.tickets = this.tickets.map((ticket) => {
+      if (ticket.id === id) {
+        return { ...ticket, status: 'closed' };
+      }
+      return ticket;
+    });
   }
 }
